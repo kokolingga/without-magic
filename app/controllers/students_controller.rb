bc22dd4963caaef7efa5eec656f78c3eacf70fd4
koko.lingga <koko.lingga@gmail.com>
2017-07-02 20:16:53 +0700

    We now have a working index and show action with associated views.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,11 @@
 class StudentsController < ApplicationController
   def index
+  	@students = Student.order(:name) # Load all students ordered by name.
+  	#Automatically load the app/views/students/index.html.erb
   end
 
   def show
+  	@student = Student.find(params[:id]) # Load student mentioned by id in the route.
+  	#Automatically load the app/views/students/show.html.erb
   end
 end
